b940dc36d696d01e4cc74d4addefdc8bac95ac72
Zexi Han <zexihan@me.com>
2017-12-14 01:20:57 -0500

    initial commit

--- gaussianKernel.py
+++ gaussianKernel.py
@@ -1,21 +1,4 @@
-# Multi-class (Nonlinear) SVM Example
-#----------------------------------
-#
-# This function wll illustrate how to
-# implement the gaussian kernel with
-# multiple classes on the iris dataset.
-#
-# Gaussian Kernel:
-# K(x1, x2) = exp(-gamma * abs(x1 - x2)^2)
-#
-# X : (Sepal Length, Petal Width)
-# Y: (I. setosa, I. virginica, I. versicolor) (3 classes)
-#
-# Basic idea: introduce an extra dimension to do
-# one vs all classification.
-#
-# The prediction of a point will be the category with
-# the largest margin or distance to boundary.
+# Gaussian Kernel
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -25,27 +8,27 @@ from tensorflow.python.framework import ops
 ops.reset_default_graph()
 
 
-with open('./txtdata/yt8m_100/train_features.txt','r') as infile:
+with open('./txtdata/yt8m_50_train_features.txt','r') as infile:
     trainFeatures = np.array(json.load(infile)).astype(np.float32)
 infile.close()
 
-with open('./txtdata/yt8m_100/validate_features.txt','r') as infile:
+with open('./txtdata/validate_features.txt','r') as infile:
     validateFeatures = np.array(json.load(infile)).astype(np.float32)
 infile.close()
 
-with open('./txtdata/yt8m_100/test_features.txt','r') as infile:
+with open('./txtdata/test_features.txt','r') as infile:
     testFeatures = np.array(json.load(infile)).astype(np.float32)
 infile.close()
 
-with open('./txtdata/yt8m_100/svm_train_labels.txt','r') as infile:
+with open('./txtdata/yt8m_50_svmtrain_labels.txt','r') as infile:
     trainLabels = np.array(json.load(infile)).astype(np.float32)
 infile.close()
 
-with open('./txtdata/yt8m_100/validate_labels.txt','r') as infile:
+with open('./txtdata/validate_labels.txt','r') as infile:
     validateLabels = np.transpose(np.array(json.load(infile)).astype(np.float32))
 infile.close()
 
-with open('./txtdata/yt8m_100/test_labels.txt','r') as infile:
+with open('./txtdata/test_labels.txt','r') as infile:
     testLabels = np.transpose(np.array(json.load(infile)).astype(np.float32))
 infile.close()
     
@@ -58,6 +41,7 @@ y_vals = np.transpose(trainLabels)
 
 # Declare batch size
 batch_size = 2048
+regulation_rate = 5e3
 
 # Initialize placeholders
 x_data = tf.placeholder(shape=[None, 1152], dtype=tf.float32)
@@ -69,12 +53,13 @@ b = tf.Variable(tf.random_normal(shape=[10, batch_size]))
 #b = tf.Variable(tf.random_normal(shape=[10]))
 
 # Gaussian (RBF) kernel
-gamma = tf.constant(-0.05)
+gamma = tf.constant(-0.09)
 dist = tf.reduce_sum(tf.square(x_data), 1)
 dist = tf.reshape(dist, [-1,1])
 sq_dists = tf.add(tf.subtract(dist, tf.multiply(2., tf.matmul(x_data, tf.transpose(x_data)))), tf.transpose(dist))
 my_kernel = tf.exp(tf.multiply(gamma, tf.abs(sq_dists)))
-#my_kernel = tf.matmul(x_data, tf.transpose(x_data))
+
+
 # Declare function to do reshape/batch multiplication
 def reshape_matmul(mat):
     v1 = tf.expand_dims(mat, 1)
@@ -88,6 +73,7 @@ y_target_cross = reshape_matmul(y_target)
 
 second_term = tf.reduce_sum(tf.multiply(my_kernel, tf.multiply(b_vec_cross, y_target_cross)),[1,2])
 loss = tf.reduce_sum(tf.negative(tf.subtract(first_term, second_term)))
+#loss += regulation_rate * tf.nn.l2_loss(b)
 
 # Gaussian (RBF) prediction kernel
 rA = tf.reshape(tf.reduce_sum(tf.square(x_data), 1),[-1,1])
@@ -112,32 +98,34 @@ optimizer = tf.train.AdamOptimizer(0.01).minimize(loss)
 init = tf.global_variables_initializer()
 sess.run(init)
 
+loss_vec = []
+train_accuracy = []
+validate_accuracy = []
+
 # Training loop
 for i in range(2500):
     rand_index = np.random.choice(len(x_vals), size=batch_size)
     rand_x = x_vals[rand_index]
     rand_y = y_vals[:,rand_index]
-    #print(rand_y)
     sess.run(optimizer, feed_dict={x_data: rand_x, y_target: rand_y})
     
     temp_loss = sess.run(loss, feed_dict={x_data: rand_x, y_target: rand_y})
+    loss_vec.append(temp_loss)
     
-    acc_temp = sess.run(accuracy, feed_dict={x_data: rand_x,
-                                             y_target: rand_y,
-                                             prediction_grid:rand_x})
-    #train_acc = sess.run(accuracy, feed_dict = {x_data: x_vals,
-    #                                            y_target: y_vals,
-    #                                            prediction_grid: x_vals})
+    train_prediction = sess.run(prediction, feed_dict={x_data: rand_x, y_target: rand_y, prediction_grid:trainFeatures})
+    train_acc = sess.run(tf.reduce_mean(tf.cast(tf.equal(train_prediction, tf.argmax(trainLabels,1)), tf.float32)))
+    train_accuracy.append(train_acc)
+
+    valid_prediction = sess.run(prediction, feed_dict={x_data: rand_x, y_target: rand_y, prediction_grid:validateFeatures})
+    valid_acc = sess.run(tf.reduce_mean(tf.cast(tf.equal(valid_prediction, tf.argmax(validateLabels,0)), tf.float32)))
+    validate_accuracy.append(valid_acc)
     if (i+1)%100==0:
         print('Step #' + str(i+1))
         print('Loss = ' + str(temp_loss))
-        print('batch_accuracy = ' + str(acc_temp))
-    #    print('train_accuracy = ' + str(train_acc))
-        valid_prediction = sess.run(prediction, feed_dict={x_data: rand_x,
-                                             y_target: rand_y,
-                                             prediction_grid:validateFeatures})
-        valid_acc = sess.run(tf.reduce_mean(tf.cast(tf.equal(valid_prediction, tf.argmax(validateLabels,0)), tf.float32)))
+        print('train_accuracy = ' + str(train_acc))
         print('validate_accuracy = ' + str(valid_acc))
 test_prediction = sess.run(prediction, feed_dict={x_data: rand_x, y_target: rand_y, prediction_grid:testFeatures})
 test_acc = sess.run(tf.reduce_mean(tf.cast(tf.equal(test_prediction, tf.argmax(testLabels, 0)), tf.float32)))
-print('test_accuray =' + str(test_acc))        
+print('test_accuray =' + str(test_acc))
+
+        
--- linearKernel.py
+++ linearKernel.py
@@ -1,30 +1,41 @@
+# Linear Kernel
+# Linear SVCs are very powerful.  
+# But sometimes the data are not very linear.  
+# To this end, we can use the 'kernel trick' to map our data into 
+# a higher dimensional space, where it may be linearly separable.  
+# Doing this allows us to separate out non-linear classes.
+
+
+
+import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 from tensorflow.python.framework import ops
 import json
 ops.reset_default_graph()
 
-with open('./txtdata/yt8m_100/train_features.txt','r') as infile:
+# data input
+with open('./txtdata/yt8m_50_train_features.txt','r') as infile:
     trainFeatures = np.array(json.load(infile)).astype(np.float32)
 infile.close()
 
-with open('./txtdata/yt8m_100/validate_features.txt','r') as infile:
+with open('./txtdata/validate_features.txt','r') as infile:
     validateFeatures = np.array(json.load(infile)).astype(np.float32)
 infile.close()
 
-with open('./txtdata/yt8m_100/test_features.txt','r') as infile:
+with open('./txtdata/test_features.txt','r') as infile:
     testFeatures = np.array(json.load(infile)).astype(np.float32)
 infile.close()
 
-with open('./txtdata/yt8m_100/svm_train_labels.txt','r') as infile:
+with open('./txtdata/yt8m_50_svmtrain_labels.txt','r') as infile:
     trainLabels = np.transpose(np.array(json.load(infile)).astype(np.float32))
 infile.close()
 
-with open('./txtdata/yt8m_100/svm_validate_labels.txt','r') as infile:
+with open('./txtdata/svm_validate_labels.txt','r') as infile:
     validateLabels = np.transpose(np.array(json.load(infile)).astype(np.float32))
 infile.close()
 
-with open('./txtdata/yt8m_100/test_labels.txt','r') as infile:
+with open('./txtdata/test_labels.txt','r') as infile:
     testLabels = np.array(json.load(infile)).astype(np.float32)
 infile.close()
 
@@ -35,10 +46,11 @@ sess = tf.Session()
 x_vals = trainFeatures
 y_vals = trainLabels
 
+# declare variables
 batch_size = 2048
 num_features = 1152
 num_labels = 10
-regulation_rate = 5e3
+regulation_rate = 5e1
 
 x_data = tf.placeholder(shape=[None, 1152], dtype=tf.float32)
 y_target = tf.placeholder(shape=[10, None], dtype=tf.float32)
@@ -47,12 +59,10 @@ prediction_grid = tf.placeholder(shape=[None, 1152], dtype=tf.float32)
 b = tf.Variable(tf.random_normal(shape=[num_labels, batch_size]))
 zeros = tf.zeros(shape=[num_labels, batch_size])
 
-#gamma = tf.constant(-100.0)
-#dist = tf.reduce_sum(tf.square(x_data), 1)
-#dist = tf.reshape(dist, [-1,1])
-#sq_dists = tf.multiply(tf.constant(2, dtype=tf.float32), tf.matmul(x_data, tf.transpose(x_data)))
-#my_kernel = tf.exp(tf.multiply(gamma, tf.abs(sq_dists)))
+# linear kernel
 my_kernel = tf.matmul(x_data, tf.transpose(x_data))
+
+# Declare function to do reshape/batch multiplication
 def reshape_matmul(mat):
     v1 = tf.expand_dims(mat, 1)
     v2 = tf.reshape(v1, [10, batch_size, 1])
@@ -63,37 +73,19 @@ b_vec_cross = tf.matmul(tf.transpose(b), b)
 y_target_cross = reshape_matmul(y_target)
 
 second_term = tf.reduce_sum(tf.multiply(my_kernel, tf.multiply(b_vec_cross, y_target_cross)),[1,2])
-#second_term = tf.reduce_sum(tf.multiply(my_kernel, tf.multiply(b_vec_cross, y_target_cross)))
 loss = tf.reduce_sum(tf.negative(tf.subtract(first_term, second_term)))
-#loss += regulation_rate * tf.nn.l2_loss(b)
+loss += regulation_rate * tf.nn.l2_loss(b)
 
-
-
+# linear prediction kernel
 pred_kernel = tf.matmul(x_data, tf.transpose(prediction_grid))
-#rA = tf.reshape(tf.reduce_sum(tf.square(x_data), 1),[-1,1])
-#rB = tf.reshape(tf.reduce_sum(tf.square(prediction_grid), 1),[-1,1])
-#pred_sq_dist = tf.add(tf.subtract(rA, tf.multiply(tf.constant(2, dtype=tf.float32), tf.matmul(x_data, tf.transpose(prediction_grid)))), tf.transpose(rB))
-#pred_kernel = tf.exp(tf.multiply(gamma, tf.abs(pred_sq_dist)))
 
+# calculate predictions
 prediction_output = tf.matmul(tf.multiply(y_target,b), pred_kernel)
 prediction = tf.arg_max(prediction_output, 0)
 accuracy = tf.reduce_mean(tf.cast(tf.equal(prediction, tf.argmax(y_target,0)), tf.float32))
 
 
-#base_learning_rate = 0.05
-#learning_rate_decay_examples = 2000
-#learning_rate_decay = 0.2
-#global_step = tf.Variable(0, trainable=False)
-#learning_rate = tf.train.exponential_decay(
-#  base_learning_rate,
-#  global_step,
-#  learning_rate_decay_examples,
-#  learning_rate_decay,
-#  staircase=True)
-#train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss, global_step=global_step)
-
-
-
+# define optimizer
 my_opt = tf.train.AdamOptimizer(0.01)
 train_step = my_opt.minimize(loss)
 
@@ -101,28 +93,59 @@ init = tf.global_variables_initializer()
 sess.run(init)
 
 loss_vec = []
-batch_accuracy = []
+train_acc = []
+valid_acc = []
+
+# start running gradient descent
 for i in range(2500):
     rand_index = np.random.choice(len(x_vals), size=batch_size)
     rand_x = x_vals[rand_index]
     rand_y = y_vals[:,rand_index]
     #rand_x = x_vals[:batch_size]
     #rand_y = y_vals[:,:batch_size]
+    
+    # feed data to optimizer batch by batch    
     sess.run(train_step, feed_dict={x_data: rand_x, y_target: rand_y})
     
     temp_loss = sess.run(loss, feed_dict={x_data: rand_x, y_target: rand_y})
     loss_vec.append(temp_loss)
     
-    acc_temp = sess.run(accuracy, feed_dict={x_data: rand_x, y_target: rand_y, prediction_grid:rand_x})
-    batch_accuracy.append(acc_temp)
+    temp_train_acc =  sess.run(tf.reduce_mean(tf.cast(tf.equal(prediction, tf.argmax(trainLabels,0)), tf.float32)) ,feed_dict={x_data: rand_x, y_target: rand_y,prediction_grid: trainFeatures})
+    train_acc.append(temp_train_acc)
+    temp_valid_acc = sess.run(tf.reduce_mean(tf.cast(tf.equal(prediction, tf.argmax(validateLabels,0)), tf.float32)) ,feed_dict={x_data: rand_x, y_target: rand_y,prediction_grid: validateFeatures})
+    valid_acc.append(temp_valid_acc)
     
+    # print out accuracy every 50 steps
     if (i+1)%50==0:
         print('Step #' + str(i+1))
         print('Loss = ' + str(temp_loss))
-        print('Mini Batch accuracy = ' + str(acc_temp))
-        acc_va = sess.run(tf.reduce_mean(tf.cast(tf.equal(prediction, tf.argmax(validateLabels,0)), tf.float32)) ,feed_dict={x_data: rand_x, y_target: rand_y,prediction_grid: validateFeatures})
-        print('Validation accuracy = ' + str(acc_va))
+        print('Mini Batch accuracy = ' + str(temp_train_acc))
+        print('Validation accuracy = ' + str(temp_valid_acc))
 
 acc_test = sess.run(tf.reduce_mean(tf.cast(tf.equal(prediction, tf.argmax(testLabels,1)), tf.float32)) ,feed_dict={x_data: rand_x, y_target: rand_y,prediction_grid: testFeatures})
 print('Test accuracy = ' + str(acc_test))      
+
+# Plot loss over time
+num_steps = 2500
+step = range(1, num_steps+1)
+epoch = [i*batch_size/46171 for i in step]
+
+plt.plot(epoch, loss_vec, 'k-')
+plt.title('Linear Kernel SVM\nDual Loss per Epoch')
+plt.xlabel('Epoch')
+plt.ylabel('Dual Loss')
+plt.savefig('./graphs/linearKernel_loss.png')
+plt.show()
+plt.close()
+    
+# Plot train and test accuracy
+plt.plot(epoch, train_acc, 'k-', label='Train Set Accuracy')
+plt.plot(epoch, valid_acc, 'r--', label='Validate Set Accuracy')
+plt.title('Linear Kernel SVM\nTrain and Validate Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend(loc='lower right')
+plt.savefig('./graphs/linearKernel_acc.png')
+plt.show()
+plt.close()
 
